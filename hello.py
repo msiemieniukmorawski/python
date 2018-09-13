693b027eb250203a0bd08abf1477a0e92923d554
MS-M <msiemieniukmorawski@gmail.com>
2018-09-13 19:57:25 +0200

    commit

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
-name = input("jak masz na imię?")
-print(name)
+hello = 'hello'
+world = 'world'
+ex = '!'
+#name = input("jak masz na imię? ").capitalize()
+
+#print(name)
+#print(hello.upper() + " " + name + ex)
+
+#number = '3'
+#print(number.isnumeric())
+
+lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate porta odio. Sed vitae neque molestie, consectetur diam lobortis, congue mauris.'
+
+print(lorem[0])
+print(lorem[6:11])
+print(lorem[:11])
+print(len(lorem))
