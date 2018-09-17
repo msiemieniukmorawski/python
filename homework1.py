997a6278d63851db792bb7f677b57789639e405b
MS-M <msiemieniukmorawski@gmail.com>
2018-09-17 20:52:36 +0200

    improvement of homework

--- homework1.py
+++ homework1.py
@@ -1,33 +1,15 @@
 string1 = "Ala ma kota, kot ma Ale"
 string2 = "Arek woli psy, a najbardziej boksery"
 
-string1_len = len(string1)
-string2_len = len(string2)
 
-if( string1_len <= string2_len ):
-    string_longer = string2_len
-    space = string2_len - string1_len
-    short_string = string1
-    long_string = string2
-else:
-    string_longer = string1_len
-    space = string1_len - string2_len
-    short_string = string2
-    long_string = string1
+#           A           R           E           K       sapce       w           o           l       i           space       p           s       y           comma       saoce     a     space      n           a        j           b           a       r           d           z         i         e           j        sapce      b           o        k          s           e           r         y
+new_string = chr(65) + chr(114) + chr(101) + chr(107) + chr(32) + chr(119) + chr(111) + chr(108) + chr(105)  + chr(32) + chr(112) + chr(115) + chr(121) + chr(44) + chr(32) + chr(97) + chr(32) + chr(110) + chr(97) + chr(106) + chr(98) + chr(97) + chr(114) + chr(100) + chr(112) + chr(105) + chr(101) + chr(106) + chr(32) + chr(98) + chr(111) + chr(107) + chr(115) + chr(101) + chr(114) + chr(121)
 
-string_longer = int(string_longer)
-short_string = short_string + (' ' * space)
-short_string =list(short_string)
-i = 0
 
-while i < string_longer:
-    if (ord(short_string[i]) != ord(long_string[i])):
-        short_string[i] = long_string[i]
+print(new_string)
 
-    i += 1
 
-short_string = ''.join(short_string)
-print(short_string)
+
 
 
 
